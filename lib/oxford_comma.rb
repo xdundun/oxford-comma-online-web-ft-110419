1b0dcd8b203972bf556cde04115c59fbe885248e
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-04 19:26:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size == 1
   puts  array.join
   elsif array.size  == 2
-    array.join(" and ")
+    puts array.join(" and ")
   else
   array.each do |names|
     array.join(",")
