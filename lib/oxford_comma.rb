a9b91b573935d4d7a50c360ee490c4450259663d
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-04 19:35:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,15 @@
 def oxford_comma(array)
-  if array.size == 1
-  return  array.join
-  elsif array.size  == 2
-    return array.join(" and ")
-  else
+#  if array.size == 1
+#  return  array.join
+  #elsif array.size  == 2
+#  return array.join(" and ")
+  #else
   #array.each do |names|
   #  puts array.join(",")
   #  if names == array[-1]
     #  puts array.join(" and ")
   #  end
   return array.to_sentence
-end
-puts array
+#end
+#puts array
 end
