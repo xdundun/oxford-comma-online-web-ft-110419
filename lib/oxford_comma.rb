94c141cf31f15447991e08b804951b5a098bfcc6
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-04 19:54:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,7 @@ def oxford_comma(array)
   elsif array.size  == 2
     return array.join(" and ")
   end
-  attendees.each_with_index.map do |attendee, index|
-     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
-   end
+
   array.map do |names|
     if names == array[-1]
       names = "and " << names
