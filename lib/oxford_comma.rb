01fdfa173ebf12986716a7ee4bb9ed53a7fb697e
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-04 20:03:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,16 +4,8 @@ def oxford_comma(array)
   elsif array.size  == 2
     return array.join(" and ")
   end
-
-#  array.map do |names|
-  #  puts names
-  #  if names == array[-1]
-  #    names = "and " << names
-  #  else
-  #    names = names << ", "
-  #  end
-  #end
-  array[-1] = "and " << array[-1]
-return array.join(", ")
-
+#access last element and shovel and before fruit
+    array[-1] = "and " << array[-1]
+#join the array with commas as necessary
+  return array.join(", ")
 end
